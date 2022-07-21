645c7ea1e80137ed68d0e12f535a7e65a7857a0d
Andy Learmouth <andymlearmouth@hotmail.com>
2022-07-21 20:10:17 +0100

    Codewars

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//Code Wars:
+
+//Complete the solution so that it reverses the string passed into it.
+
+function solution(str){
+    return str.split('').reverse().join('')
+}
+
+solution('Hello')
