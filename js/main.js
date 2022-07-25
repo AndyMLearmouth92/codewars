247273956b469e248909ad92356a80c43f2faafb
Andy Learmouth <andymlearmouth@hotmail.com>
2022-07-25 18:59:46 +0100

    Reverse numbers into a new array

--- js/main.js
+++ js/main.js
@@ -22,3 +22,9 @@ function sum (numbers) {
 function opposite(number) {
     return -number
   }
+
+  //Convert number to reversed array of digits
+
+  function digitize(n) {
+    return n.toString().split('').reverse().map((x)=> Number(x))
+  }
