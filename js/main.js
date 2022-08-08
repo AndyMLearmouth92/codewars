46ec547b0a389407c4bf71b76efd0b03e7444e1f
Andy Learmouth <andymlearmouth@hotmail.com>
2022-08-08 20:28:09 +0100

    codewars

--- js/main.js
+++ js/main.js
@@ -87,3 +87,19 @@ const boolToWord = bool => bool ? 'Yes' : 'No'
 function descendingOrder(n){
   return Number(n.toString().split('').sort((a, b)=> b - a).join(''))
 }
+
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+// The output should be two capital letters with a dot separating them.
+
+// It should look like this:
+
+// Sam Harris => S.H
+
+// patrick feeney => P.F
+
+function abbrevName(name){
+
+  var nameArray = name.split(" ");
+  return (nameArray[0][0] + "." + nameArray[1][0]).toUpperCase();
+}
