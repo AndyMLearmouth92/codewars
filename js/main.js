d972a304e032a6bd886ec2df53ece497d572b655
Andy Learmouth <andymlearmouth@hotmail.com>
2022-08-12 16:51:48 +0100

    CW

--- js/main.js
+++ js/main.js
@@ -121,3 +121,13 @@ function doubleInteger(i) {
 function isDivisible(n, x, y) {
   return (n % x === 0 && n % y ===0)? true : false
 }
+
+//with an if statement
+
+function isDivisible(n, x, y) {
+  if(n % x === 0 && n % y === 0){
+    return true
+  }else{
+    return false
+}
+  }
