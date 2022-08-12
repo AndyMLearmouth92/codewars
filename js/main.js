b15b7f46f52ba16570f9cfe41cbf9edbabc5b890
Andy Learmouth <andymlearmouth@hotmail.com>
2022-08-12 16:46:37 +0100

    CW

--- js/main.js
+++ js/main.js
@@ -115,3 +115,9 @@ function maps(x){
 function doubleInteger(i) {
   return i*2
 }
+
+//Create a function that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero digits.
+
+function isDivisible(n, x, y) {
+  return (n % x === 0 && n % y ===0)? true : false
+}
