7cdccb29b138bfdee2c339edffd371dc7513b4a7
Andy Learmouth <andymlearmouth@hotmail.com>
2022-08-12 17:13:55 +0100

    CW

--- js/main.js
+++ js/main.js
@@ -131,3 +131,13 @@ function isDivisible(n, x, y) {
     return false
 }
   }
+
+  //Clock shows h hours, m minutes and s seconds after midnight. //Your task is to write a function which returns the time since midnight in milliseconds.
+
+  function past(h, m, s){
+    let hours = h * 60 * 60 * 1000;
+    let minutes = m * 60 * 1000;
+    let seconds = s * 1000;
+    
+    return hours + minutes + seconds;
+  }
