0b4438fd7e3ac0f7201e183a3f6777a62368269c
Andy Learmouth <andymlearmouth@hotmail.com>
2022-08-13 20:25:39 +0100

    CW

--- js/main.js
+++ js/main.js
@@ -141,3 +141,15 @@ function isDivisible(n, x, y) {
     
     return hours + minutes + seconds;
   }
+
+
+  //Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+
+
+  function paperwork(n, m) {
+    if(n >= 0 && m >= 0){
+      return n*m
+    } else if(n < 0 || m < 0){
+      return 0
+    }
+  }
