719886c82a1ee64827f7a776a12ff4ade93e1174
Andy Learmouth <andymlearmouth@hotmail.com>
2022-08-25 17:59:19 +0100

    CW

--- js/main.js
+++ js/main.js
@@ -250,3 +250,15 @@ function smallestIntegerFinder(arr){
       }
     return average;
   }
+
+  //In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+  function filter_list(l) {
+    let newArr = []
+    for(let i = 0; i < l.length; i++){
+      if(typeof(l[i]) === 'number'){
+        newArr.push(l[i])
+      }
+    }
+      return newArr
+  }
