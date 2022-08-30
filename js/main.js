df592c6548ace1516ac46655fb0031bca1108c08
Andy Learmouth <andymlearmouth@hotmail.com>
2022-08-30 20:22:04 +0100

    CW

--- js/main.js
+++ js/main.js
@@ -262,3 +262,10 @@ function smallestIntegerFinder(arr){
     }
       return newArr
   }
+
+
+  //Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+  function invert(array) {
+    return array.map(x => x * -1);
+ }
