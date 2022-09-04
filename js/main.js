d8700152b1678dbc095cebfd2a8c57a07b4a2580
Andy Learmouth <andymlearmouth@hotmail.com>
2022-09-04 13:43:54 +0100

    CW

--- js/main.js
+++ js/main.js
@@ -417,3 +417,9 @@ function solution(nums){
       return nums !== null ? nums.sort((a, b) => a - b) : []
     
     }
+
+    //Write a function that takes an array of strings as an argument and returns a sorted array containing the same strings, ordered from shortest to longest.
+
+    function sortByLength (array) {
+      return array.sort((a,b) => a.length - b.length)
+    };
