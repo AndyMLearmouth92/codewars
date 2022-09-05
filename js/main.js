8eb987d0a7fd146aac2d245a6b8002492424f581
Andy Learmouth <andymlearmouth@hotmail.com>
2022-09-05 18:17:01 +0100

    CW

--- js/main.js
+++ js/main.js
@@ -423,3 +423,18 @@ function solution(nums){
     function sortByLength (array) {
       return array.sort((a,b) => a.length - b.length)
     };
+
+
+    //Given two numbers and an arithmetic operator (the name of it, as a string), return the result of the two numbers having that operator used on them.
+
+    function arithmetic(a, b, operator){
+      if(operator === 'add'){
+        return a+b
+      }else if(operator === 'subtract'){
+        return a-b
+      }else if(operator === 'multiply'){
+        return a*b
+      }else if(operator === 'divide'){
+        return a/b
+      }
+    }
