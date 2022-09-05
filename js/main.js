d2245fb4f27960418ac317bad462b9a3b38c78ac
Andy Learmouth <andymlearmouth@hotmail.com>
2022-09-05 18:18:11 +0100

    CW'
    git add .
    
    pwd
    
    restart

--- js/main.js
+++ js/main.js
@@ -438,3 +438,12 @@ function solution(nums){
         return a/b
       }
     }
+
+    //OR
+
+    function arithmetic(a, b, operator){
+      if(operator === 'add') return a+b
+      if(operator === 'subtract') return a-b
+      if(operator === 'multiply') return a*b
+      if(operator === 'divide') return a/b
+  }
