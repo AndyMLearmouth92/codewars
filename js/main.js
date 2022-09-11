18c16849f696f9816dcf2910f09be32fa3ac4e2b
Andy Learmouth <andymlearmouth@hotmail.com>
2022-09-11 17:31:09 +0100

    CW

--- js/main.js
+++ js/main.js
@@ -606,6 +606,13 @@ function getAverage(marks){
   return Math.floor(total / marks.length)
 }
 
+//Level 7
+//Given a two-dimensional array of integers, return the flattened version of the array with all the integers in the sorted (ascending) order.
+
+function flattenAndSort(array) {
+  return array.flat().sort((a, b) => a - b)
+  }
+
 
 
 
