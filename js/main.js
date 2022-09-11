b836d919fc2fa641a9f289fec90f908bf21ca0a6
Andy Learmouth <andymlearmouth@hotmail.com>
2022-09-11 08:58:35 +0100

    CW

--- js/main.js
+++ js/main.js
@@ -537,3 +537,15 @@ function sumMix(x){
     });
      return nums.reduce((sum, current) => sum + current, 0)
     }
+
+    //Level 7 array HW
+
+// JavaScript Arrays support a filter function (starting in JavaScript 1.6). Use the filter functionality to complete the function given.
+
+// The solution would work like the following:
+
+// getEvenNumbers([2,4,5,6]) // should == [2,4,6]
+
+function getEvenNumbers(numbersArray){
+  return numbersArray.filter(number => number %2 == 0)
+}
