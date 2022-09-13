5f14f21df34822434bc2f63a447acf4b397a604e
Andy Learmouth <andymlearmouth@hotmail.com>
2022-09-13 16:52:54 +0100

    CW

--- js/main.js
+++ js/main.js
@@ -671,3 +671,11 @@ function largestPairSum (numbers) {
   let sorted = numbers.sort((a,b) => b-a)
   return sorted[0] + sorted [1]
 }
+
+//Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+
+// input: names - unsorted array
+// output: sorted array
+sortme = function( names ){
+  return names.sort()
+}
