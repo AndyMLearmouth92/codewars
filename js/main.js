3c66207616881314488c3ce2aaa7d0fb4f0d9a00
Andy Learmouth <andymlearmouth@hotmail.com>
2022-09-14 14:01:07 +0100

    CW

--- js/main.js
+++ js/main.js
@@ -709,3 +709,9 @@ function toNumberArray(stringarray){
 function toNumberArray(stringarray){
   return stringarray.map(parseFloat)
 }
+
+//Make the double() function return a new array with each value twice as large as the corresponding value in the passed in array. Your solution must use the map() function of the built-in javascript Array object. If you're not familiar with map(), reading over the documentation may help.
+
+function double(array) {
+  return array.map((e) => e*2)
+}
