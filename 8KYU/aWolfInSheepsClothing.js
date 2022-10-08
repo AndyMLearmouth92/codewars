3faf6177c7d275a505197f99d74f1f861cbe823c
Andy Learmouth <andymlearmouth@hotmail.com>
2022-10-08 11:12:49 +0100

    Your commit message

--- 8KYU/aWolfInSheepsClothing.js
+++ 8KYU/aWolfInSheepsClothing.js
@@ -37,3 +37,8 @@ function warnTheSheep(queue) {
     }
 
 // Solution 2
+
+function warnTheSheep(queue) {
+    const position = queue.reverse().indexOf('wolf');
+    return position === 0 ? 'Pls go away and stop eating my sheep' : `Oi! Sheep number ${ position }! You are about to be eaten by a wolf!`;
+  }
