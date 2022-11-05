e9f2813b31d32a05cc8d5b70fe50a25eac9cb3d9
Andy Learmouth <108182837+AndyMLearmouth92@users.noreply.github.com>
2022-11-05 11:06:09 +0000

    Update Multiples of 3 or 5.js

--- 6KYU/Multiples of 3 or 5.js
+++ 6KYU/Multiples of 3 or 5.js
@@ -1,16 +1,16 @@
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
-Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. Additionally, if the number is negative, return 0 (for languages that do have them).
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. Additionally, if the number is negative, return 0 (for languages that do have them).
 
-Note: If the number is a multiple of both 3 and 5, only count it once.
+// Note: If the number is a multiple of both 3 and 5, only count it once.
 
-Starter code:
+// Starter code:
 
-function solution(number){
+// function solution(number){
   
-}
+// }
 
-Solution 1:
+// Solution 1:
 
 function solution(number){
   //Variable to store the count
@@ -29,7 +29,7 @@ function solution(number){
 return sum
 }
 
-Solution 2:
+// Solution 2:
 
 function solution(number){
  let sum = 0
