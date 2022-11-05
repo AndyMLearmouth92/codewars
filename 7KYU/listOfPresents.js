abd7b7cf5252cc1fe5d90226ecae2af7bfabdc87
Andy Learmouth <108182837+AndyMLearmouth92@users.noreply.github.com>
2022-11-05 11:31:28 +0000

    Update and rename List of Presents to listOfPresents.js

--- 7KYU/listOfPresents.js
+++ 7KYU/listOfPresents.js
@@ -1,4 +1,4 @@
-Leo's girlfriend asked him to buy a gift list during his next trip, now he wants to know how many of them will he be able to buy.
+/* Leo's girlfriend asked him to buy a gift list during his next trip, now he wants to know how many of them will he be able to buy.
 
 Write a function to help Leo out. The first parameter of the function is Leo's budget; the second one is an array containing the price of each gift. You should return an integer representing the maximum amount of gifts Leo can buy.
 
@@ -17,7 +17,9 @@ function howManyGifts(maxBudget, gifts){
   // your code goes here
 }
 
-Solution 1:
+*/
+
+// Solution 1:
 
 function howManyGifts(maxBudget, gifts){
    let sorted = gifts.sort((a,b)=>a-b)
@@ -30,7 +32,7 @@ function howManyGifts(maxBudget, gifts){
    return i-1
 }
 
-Solution 2:
+// Solution 2:
 
 function howManyGifts(maxBudget, gifts){
 return gifts.sort((a,b)=>a-b).filter(x => (maxBudget -= x) >= 0).length
