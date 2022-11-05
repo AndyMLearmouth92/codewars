7d3087e103190c557c68b94267704bbc1a661d83
Andy Learmouth <108182837+AndyMLearmouth92@users.noreply.github.com>
2022-11-05 11:37:50 +0000

    Update and rename Sum even numbers to sumEvenNumbers.js

--- 7KYU/sumEvenNumbers.js
+++ 7KYU/sumEvenNumbers.js
@@ -1,4 +1,4 @@
-Complete the function that takes a sequence of numbers as single parameter. Your function must return the sum of the even values of this sequence.
+/* Complete the function that takes a sequence of numbers as single parameter. Your function must return the sum of the even values of this sequence.
 
 Only numbers without decimals like 4 or 4.0 can be even.
 
@@ -14,19 +14,20 @@ Starter code:
 function sumEvenNumbers(input) {
   // [...]
 }
+*/
 
-Solution 1:
+// Solution 1:
 
 function sumEvenNumbers(input) {
   return input.filter(x => x % 2 == 0).reduce((sum, current) => sum + current, 0)
 }
 
-Solution 2:
+// Solution 2:
 
 const sumEvenNumbers = input => input.filter(x => x % 2 == 0).reduce((sum, current) => sum + current, 0)
 
 
-Solution 3:
+// Solution 3:
 
 function sumEvenNumbers(input) {
   let newArr = []
