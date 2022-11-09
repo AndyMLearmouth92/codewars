318d11fbe5122b279089623d670fdaa5f8cbaa23
Andy Learmouth <108182837+AndyMLearmouth92@users.noreply.github.com>
2022-11-09 19:33:49 +0000

    Update stopGninnipSMySdrow.js

--- 6KYU/stopGninnipSMySdrow.js
+++ 6KYU/stopGninnipSMySdrow.js
@@ -19,3 +19,7 @@ function spinWords(string){
 function spinWords(string){
   return string.split(' ').map(x => x.length >= 5 ? x.split('').reverse().join('') : x).join(' ')
 }
+
+// Solution 2:
+
+const spinWords = string => string.split(' ').map(x => x.length >= 5 ? x.split('').reverse().join('') : x).join(' ')
