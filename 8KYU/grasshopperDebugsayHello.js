f5968667a4833ba46b95bd69abd5315f0225ca0f
Andy Learmouth <108182837+AndyMLearmouth92@users.noreply.github.com>
2022-11-15 22:23:16 +0000

    Create grasshopperDebugsayHello.js

---
+++ 8KYU/grasshopperDebugsayHello.js
@@ -0,0 +1,21 @@
+/* Debugging sayHello function
+
+The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+Example output:
+
+Hello, Mr. Spock
+
+Starter code:
+
+function sayHello(name) {
+  return 'Hello'
+}
+
+*/
+
+// Solution 1:
+
+function sayHello(name) {
+  return `Hello, ${name}`
+}
