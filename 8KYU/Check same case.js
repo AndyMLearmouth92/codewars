d12ab5e4716ba69c3d9d658bb2486da46973872e
Andy Learmouth <108182837+AndyMLearmouth92@users.noreply.github.com>
2022-12-01 17:51:40 +0000

    Update Check same case.js

--- 8KYU/Check same case.js
+++ 8KYU/Check same case.js
@@ -34,3 +34,18 @@ function sameCase(a, b){
     return 0
   }
 }
+
+// Solution 2:
+
+function sameCase(a, b) {
+  if (!a.match(/^[A-Za-z]$/) || !b.match(/^[A-Za-z]$/)) {
+    return -1
+  } else if (
+    (a.match(/^[A-Z]$/) && b.match(/^[A-Z]$/)) ||
+    (a.match(/^[a-z]$/) && b.match(/^[a-z]$/))
+  ) {
+    return 1
+  } else {
+    return 0
+  }
+}
