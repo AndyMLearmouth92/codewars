fc90596ea622d328a94da13a3d3fc8b0d0a87f59
Andy Learmouth <andymlearmouth@hotmail.com>
2022-12-11 17:09:08 +0000

    CW reduce

---
+++ 8KYU/beginnerReduceButGrow.js
@@ -0,0 +1,15 @@
+/* Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+[1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+Starter code:
+
+function grow(x){
+
+}
+
+*/
+
+// Solution 1:
+
+const grow = (x) => x.reduce((sum, current) => sum * current)
