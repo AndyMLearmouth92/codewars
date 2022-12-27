63b6a40349d0a1b1e05b7d2d3772b35768b6d6a8
Andy Learmouth <andymlearmouth@hotmail.com>
2022-12-27 18:31:48 +0000

    Reverse words CW solution 1

---
+++ 8KYU/reversedWords.js
@@ -0,0 +1,19 @@
+/* Complete the solution so that it reverses all of the words within the string passed in.
+
+Example(Input --> Output):
+
+"The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+
+Starter code:
+
+function reverseWords(str){
+    return str; // reverse those words
+  }
+
+  */
+
+//   Solution 1:
+
+  function reverseWords(str){
+    return str.split(' ').reverse().join(' ')
+  }
