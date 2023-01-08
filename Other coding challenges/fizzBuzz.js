4a851ecfb23f0bb84782ea7a9b3b9e29eaeebc9d
Andy Learmouth <andymlearmouth@hotmail.com>
2023-01-08 10:00:12 +0000

    FizzBuzz

---
+++ Other coding challenges/fizzBuzz.js
@@ -0,0 +1,19 @@
+/*
+write a function that takes in a number (n) and console logs 1 to n, if the number is a multiple of 3 you should log 'Fizz, if the number is a multiple of 5 you should log 'Buzz' and if the number is a multiple of both 3 and 5 you should log 'Fizzbuzz'
+*/
+
+function fizzBuzz(num){
+    for(let i = 1; i < num; i++){
+        if(num % 3 == 0 && num % 5 == 0){
+            console.log('Fizz Buzz')
+        }else if(num % 3 == 0){
+            console.log('Fizz')
+        }else if(num % 5 == 0){
+            console.log('Buzz')
+        }else{
+            console.log('num')
+        }
+    }
+}
+
+fizzBuzz(100)
