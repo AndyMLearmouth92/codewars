ddea46452bdb1d1fb7031060f77f5166ac2f7a05
Andy Learmouth <andymlearmouth@hotmail.com>
2023-01-08 10:00:55 +0000

    fizzBuzz

--- Other coding challenges/fizzBuzz.js
+++ Other coding challenges/fizzBuzz.js
@@ -4,11 +4,11 @@ write a function that takes in a number (n) and console logs 1 to n, if the numb
 
 function fizzBuzz(num){
     for(let i = 1; i < num; i++){
-        if(num % 3 == 0 && num % 5 == 0){
+        if(i % 3 == 0 && i % 5 == 0){
             console.log('Fizz Buzz')
-        }else if(num % 3 == 0){
+        }else if(i % 3 == 0){
             console.log('Fizz')
-        }else if(num % 5 == 0){
+        }else if(i % 5 == 0){
             console.log('Buzz')
         }else{
             console.log('num')
