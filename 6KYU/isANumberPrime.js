f8dfa8b8fcb517ee91f8237408bd4cd2ba8d325a
Andy Learmouth <andymlearmouth@hotmail.com>
2023-01-17 17:54:52 +0000

    Is a number prime? CW S2

--- 6KYU/isANumberPrime.js
+++ 6KYU/isANumberPrime.js
@@ -33,3 +33,14 @@ function isPrime(num) {
     return true;
   }
 
+//   Solution 2:
+
+function isPrime(num) {
+    for(let i = 2; i <= Math.sqrt(num); i++) {
+      if(num % i === 0) {
+        return false;
+      }
+    }
+    return num > 1
+  }
+
