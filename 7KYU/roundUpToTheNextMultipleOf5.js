fc9a59ce5d44a42a4f0806227db7413b60a82c34
Andy Learmouth <andymlearmouth@hotmail.com>
2023-01-20 17:15:22 +0000

    Round up to the next multiple of 5 CW S2

--- 7KYU/roundUpToTheNextMultipleOf5.js
+++ 7KYU/roundUpToTheNextMultipleOf5.js
@@ -29,6 +29,14 @@ function roundToNext5(n){
     return Math.ceil(n/5) * 5
   }
 
+// Solution 2:
 
+function roundToNext5(n){
+    for(let i = n; i < n+5; i++){
+      if(i % 5 == 0){
+        return i
+      }
+    }
+  }
 
 
