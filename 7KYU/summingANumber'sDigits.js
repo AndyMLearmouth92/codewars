7702ccecd79128941629f7ffad0a2db0c6c9a00a
Andy Learmouth <andymlearmouth@hotmail.com>
2023-01-22 15:14:09 +0000

    Summing a number's digits CW S1

---
+++ 7KYU/summingANumber'sDigits.js
@@ -0,0 +1,22 @@
+/* Write a function named sumDigits which takes a number as input and returns the sum of the absolute value of each of the number's decimal digits.
+
+For example: (Input --> Output)
+
+10 --> 1
+99 --> 18
+-32 --> 5
+Let's assume that all numbers in the input will be integer values.
+
+Starter code:
+
+function sumDigits(number) {
+}
+
+*/
+
+// Solution 1:
+
+function sumDigits(number) {
+    return Math.abs(number).toString().split('').reduce((sum, current) => Number(sum) + Number(current), 0)
+  }
+
