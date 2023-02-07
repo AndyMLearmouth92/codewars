244054e8d11fef0616a55f1d1d4d64ee520bee77
Andy Learmouth <andymlearmouth@hotmail.com>
2023-02-07 16:41:56 +0000

    String transformer CW Solution 3

--- 6KYU/stringTransformer.js
+++ 6KYU/stringTransformer.js
@@ -29,3 +29,17 @@ function stringTransformer(str) {
     let reversedWord = reversedCase.split(' ').reverse().join(' ')
     return reversedWord
   }
+
+// Solution 3:
+
+function stringTransformer(str) {
+    let result = ''
+    for(let i = 0; i < str.length; i++){
+      if(str[i] === str[i].toUpperCase()){
+        result += str[i].toLowerCase()
+      }else{
+        result += str[i].toUpperCase()
+      }
+     }
+    return result.split(' ').reverse().join(' ')
+}
