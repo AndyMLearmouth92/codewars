840fff382f0b81b9a54e549b0bbd9f4a8097ec88
Andy Learmouth <andymlearmouth@hotmail.com>
2023-02-22 12:21:26 +0000

    Switcheroo CW S2

--- 7KYU/switcheroo.js
+++ 7KYU/switcheroo.js
@@ -15,3 +15,19 @@ function switcheroo(x){
 // Solution 1:
 
 const switcheroo = x => x.split('').map(e => e == 'a' ? 'b': e == 'b' ? 'a' : e).join('')
+
+// Solution 2:
+
+function switcheroo(x){
+    let output = ''
+    for(let i = 0; i < x.length; i++){
+        if(x[i] == 'a'){
+        output += 'b'
+        }else if(x[i] == 'b'){
+        output += 'a'
+        }else{
+        output += 'c'
+        }
+  }
+    return output
+  }
