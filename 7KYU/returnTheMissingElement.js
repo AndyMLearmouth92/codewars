f9df2cd6bd4535673bdc935345d83f64afd5b304
Andy Learmouth <andymlearmouth@hotmail.com>
2023-03-15 16:23:28 +0000

    Return the Missing Element CW S2

--- 7KYU/returnTheMissingElement.js
+++ 7KYU/returnTheMissingElement.js
@@ -28,3 +28,13 @@ function getMissingElement(superImportantArray){
       }
     }
   }
+
+//   Solution 2:
+
+function getMissingElement(superImportantArray){
+    for(let i = 0; i < 10; i++){
+      if(superImportantArray.indexOf(i) == -1){
+        return i
+      }
+    }
+  }
