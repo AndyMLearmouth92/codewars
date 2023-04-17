130c47fd624ce782646c4fa298b9c7d7abea3269
Andy Learmouth <andymlearmouth@hotmail.com>
2023-04-17 11:31:25 +0100

    Valid Parentheses CW S2

--- 7KYU/returnAStringsEvenCharacters.js
+++ 7KYU/returnAStringsEvenCharacters.js
@@ -16,10 +16,24 @@ function evenChars(string) {
 // Solution:
 
 function evenChars(string) {
-    let inputArr = string.split('')
-    if(inputArr.length < 2 || inputArr.length > 100){
-      return 'invalid string'
-    }else{
-      return inputArr.filter((e,i) => i % 2 !== 0)
-    }
+  let inputArr = string.split("");
+  if (inputArr.length < 2 || inputArr.length > 100) {
+    return "invalid string";
+  } else {
+    return inputArr.filter((e, i) => i % 2 !== 0);
   }
+}
+
+// Solution 2:
+
+function validParentheses(parenStr) {
+  let counter = 0;
+
+  for (let char of parenStr) {
+    if (char === "(") counter++;
+    if (char === ")") counter--;
+
+    if (counter < 0) return false;
+  }
+  return counter === 0;
+}
