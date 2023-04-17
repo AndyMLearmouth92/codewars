f6c950b904b451a287edbc0bf779003946693042
Andy Learmouth <andymlearmouth@hotmail.com>
2023-04-17 11:35:57 +0100

    Valid Parentheses CW S2

--- 7KYU/validParentheses.js
+++ 7KYU/validParentheses.js
@@ -41,3 +41,17 @@ function validParentheses(parenStr) {
   }
   return stack.length === 0;
 }
+
+// Solution 2:
+
+function validParentheses(parenStr) {
+  let counter = 0;
+
+  for (let char of parenStr) {
+    if (char === "(") counter++;
+    if (char === ")") counter--;
+
+    if (counter < 0) return false;
+  }
+  return counter === 0;
+}
