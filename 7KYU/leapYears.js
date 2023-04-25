d0335fa6460edf530b943e92f8b9c00dc31b1866
Andy Learmouth <andymlearmouth@hotmail.com>
2023-04-25 17:03:11 +0100

    Leap Years CW S2

--- 7KYU/leapYears.js
+++ 7KYU/leapYears.js
@@ -20,3 +20,14 @@ function isLeapYear(year) {
 function isLeapYear(year) {
     return (year % 100 !== 0 && year % 4 === 0) || year % 400 === 0;
   }
+
+// Solution 2:
+
+function isLeapYear(year) {
+    if(year % 4 === 0 && year % 100 !== 0){
+      return true
+    }else if(year % 400 === 0){
+      return true
+    }
+    return false
+  }
