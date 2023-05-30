5400d87b9ff87b9ffad18755989411cabbc37370
Andy Learmouth <andymlearmouth@hotmail.com>
2023-05-30 09:17:15 +0100

    Split Strings CW Solution 1

---
+++ 6KYU/splitStrings.js
@@ -0,0 +1,19 @@
+/* Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+
+Examples:
+
+* 'abc' =>  ['ab', 'c_']
+* 'abcdef' => ['ab', 'cd', 'ef']
+
+Starter code:
+
+function solution(str){
+   
+}
+*/
+
+// Solution 1:
+
+function solution(s){
+    return (s+"_").match(/.{2}/g)||[]
+ }
