28487bcc328ee560e49fea5867f6fc4ba0ef20d1
Andy Learmouth <andymlearmouth@hotmail.com>
2023-05-31 16:51:31 +0100

    Sort the odd CW S2

--- 6KYU/sortTheOdd.js
+++ 6KYU/sortTheOdd.js
@@ -21,3 +21,14 @@ function sortArray(array) {
     let odd = array.filter(e => e % 2 !== 0).sort((a,b) => a-b)
     return array.map(e => e % 2 !== 0 ? odd.shift() : e)
   }
+
+// Solution 2:
+
+function sortArray(array) {
+    let indices = [];
+    array
+        .filter((e, i) => e % 2 && indices.push(i))
+        .sort((a, b) => a - b)
+        .forEach((e, i) => array[indices[i]] = e);
+      return array;
+    }
