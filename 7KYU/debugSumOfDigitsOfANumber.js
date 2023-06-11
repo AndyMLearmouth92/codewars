1858636678f1dec9a81ece08398f011470e82cff
Andy Learmouth <andymlearmouth@hotmail.com>
2023-06-11 17:24:21 +0100

    Debug Sum of Digits of a Number CW S1

---
+++ 7KYU/debugSumOfDigitsOfANumber.js
@@ -0,0 +1,31 @@
+/* Debug   function getSumOfDigits that takes positive integer to calculate sum of its digits. Assume that argument is an integer.
+
+Example
+
+123  => 6
+223  => 7
+1337 => 14
+
+Starter code:
+
+function getSumOfDigits(integer) {
+  var sum = null;
+  var digits =  Math.floor(integer).toString();
+  for(var ix = 1; ix < digits.length; ix = sum + 1) {
+    sum =+ digits[ix + 1]);
+  }
+  return sum;
+}
+
+*/
+
+// Solution 1:
+
+function getSumOfDigits(integer) {
+    let sum = 0;
+    let digits =  Math.floor(integer).toString();
+    for(var i = 0; i < digits.length; i++) {
+      sum += Number(digits[i]);
+    }
+    return sum
+  }
