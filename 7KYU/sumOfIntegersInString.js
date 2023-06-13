f3fbe6cb87c3548189c8c2db7173aa404b0d4246
Andy Learmouth <andymlearmouth@hotmail.com>
2023-06-13 17:04:29 +0100

    Sum of integers in string CW S1

---
+++ 7KYU/sumOfIntegersInString.js
@@ -0,0 +1,17 @@
+/* Your task in this kata is to implement a function that calculates the sum of the integers inside a string. For example, in the string "The30quick20brown10f0x1203jumps914ov3r1349the102l4zy dog", the sum of the integers is 3635.
+
+Note: only positive integers will be tested.
+
+Starter code:
+
+function sumOfIntegersInString(s){
+  return // what the function name says
+}
+*/
+
+// Solution 1:
+
+function sumOfIntegersInString(s){
+    let nums = s.split(/([^0-9])/g).map(e => parseInt(e)).filter(Boolean)
+    return nums.reduce((sum, current) => sum + current, 0)
+  }
