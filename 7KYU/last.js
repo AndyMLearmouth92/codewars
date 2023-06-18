f196349272c2f2d0ab60c22575a80a0eeb115109
Andy Learmouth <andymlearmouth@hotmail.com>
2023-06-18 09:52:40 +0100

    Last CW Solution

---
+++ 7KYU/last.js
@@ -0,0 +1,23 @@
+/* Find the last element of the given argument(s).
+
+Examples
+
+last([1, 2, 3, 4] ) // =>  4
+last("xyz")         // => "z"
+last(1, 2, 3, 4)    // =>  4
+In javascript and CoffeeScript a list will be an array, a string or the list of arguments.
+
+Starter code:
+
+function last(list){
+
+}
+*/
+
+// Solution:
+
+function last(list) {
+    let lastArgument = arguments[arguments.length-1];
+    let lastElement = list[list.length-1];
+    return arguments.length > 1 ? lastArgument : lastElement || lastArgument;
+  }
