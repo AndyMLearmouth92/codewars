7a717cd6ee4e06c56919fb8256a103cab2c0046b
Andy Learmouth <andymlearmouth@hotmail.com>
2023-07-08 12:06:53 +0100

    Reversed Words Solution 1

---
+++ Other coding challenges/reverseWord.js
@@ -0,0 +1,11 @@
+//Given a string of characters as input, write a function that returns it with the characters reversed. No methods
+
+// Solution 1:
+
+function reverseWord(word){
+    let reversedWord = ''
+    for(let i = word.length-1; i > -1; i--){
+        reversedWord += word[i]
+    }
+    console.log(reversedWord)
+}
