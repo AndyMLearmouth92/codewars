49e7fa738035b40e30b936874334de028cad4e6a
Andy Learmouth <andymlearmouth@hotmail.com>
2023-07-13 18:07:24 +0100

    Absent Vowel CW Solution 3

--- 7KYU/absentVowel.js
+++ 7KYU/absentVowel.js
@@ -41,3 +41,19 @@ function absentVowel(x){
     }
   }
 
+//   Solution 3:
+
+  function absentVowel(x){
+    if(x.indexOf('a') === -1){
+      return 0
+    }else if(x.indexOf('e') === -1){
+      return 1
+    }else if(x.indexOf('i') === -1){
+      return 2
+    }else if(x.indexOf('o') === -1){
+      return 3
+    }else if(x.indexOf('u') === -1){
+      return 4
+    }
+}
+
