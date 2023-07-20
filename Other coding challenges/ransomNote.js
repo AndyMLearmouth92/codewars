87da6b68aa7c11c2211dea65fbec941f2ce554a0
Andy Learmouth <andymlearmouth@hotmail.com>
2023-07-20 16:52:45 +0100

    Ransome note solution 2

--- Other coding challenges/ransomNote.js
+++ Other coding challenges/ransomNote.js
@@ -29,3 +29,27 @@ function cutOutRansom(magazine, ransom) {
   }
   return true;
 }
+
+// Solution 2:
+
+const ransomNote = (note, magazine) => {
+  const magazineWords = magazine.split(" ");
+  const magazineHash = {};
+
+  magazineWords.forEach((word) => {
+    if (!magazineHash[word]) magazineHash[word] = 0;
+    magazineHash[word]++;
+  });
+
+  const noteWords = note.split(" ");
+  let possible = true;
+
+  noteWords.forEach((word) => {
+    if (magazineHash[word]) {
+      magazineHash[word]--;
+      if (magazineHash[word] < 0) possible = false;
+    } else possible = false;
+  });
+
+  return possible;
+};
