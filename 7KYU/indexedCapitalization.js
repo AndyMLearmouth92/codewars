67eba118c309c3e0360062186671efe939ee11d2
Andy Learmouth <andymlearmouth@hotmail.com>
2023-08-03 20:32:28 +0100

    Indexed capitalization CW Solution.

---
+++ 7KYU/indexedCapitalization.js
@@ -0,0 +1,34 @@
+/* Given a string and an array of integers representing indices, capitalize all letters at the given indices.
+
+For example:
+
+capitalize("abcdef",[1,2,5]) = "aBCdeF"
+capitalize("abcdef",[1,2,5,100]) = "aBCdeF". There is no index 100.
+The input will be a lowercase string with no spaces and an array of digits.
+
+Good luck!
+
+Be sure to also try:
+
+Alternate capitalization
+
+String array revisal
+
+Starter code:
+
+function capitalize(s,arr){
+  return "";
+};
+*/
+
+// Solution:
+
+function capitalize(s, arr) {
+  let result = s.split("");
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < result.length) {
+      result[arr[i]] = result[arr[i]].toUpperCase();
+    }
+  }
+  return result.join("");
+}
