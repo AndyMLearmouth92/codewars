55bbb0568d44bd18afcb346c3c5ecbd5dd447d61
Andy Learmouth <andymlearmouth@hotmail.com>
2023-08-11 09:33:50 +0100

    Function 2 - squaring an argument CW Solution

---
+++ 8KYU/function2SquaringAnArgument.js
@@ -0,0 +1,13 @@
+/* Now you have to write a function that takes an argument and returns the square of it.
+
+Starter code:
+
+// Write the "square"-function here
+
+*/
+
+// Solution:
+
+function square(num){
+    return num * num
+   }
