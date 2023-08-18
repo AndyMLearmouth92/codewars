3f2274d5940818ff9ba1996c886858da4ed6b413
Andy Learmouth <andymlearmouth@hotmail.com>
2023-08-18 19:14:04 +0100

    Name Array Capping CW S1

---
+++ 7KYU/nameArrayCapping.js
@@ -0,0 +1,19 @@
+/* Create a method that accepts an array of names, and returns an array of each name with its first letter capitalized.
+
+example
+
+capMe(['jo', 'nelson', 'jurie'])     // returns ['Jo', 'Nelson', 'Jurie']
+capMe(['KARLY', 'DANIEL', 'KELSEY']) // returns ['Karly', 'Daniel', 'Kelsey']
+
+Starter code:
+
+function capMe(names) {
+
+}
+*/
+
+// Solution:
+
+function capMe(names) {
+  return names.map(e => e[0].toUpperCase()+e.slice(1).toLowerCase())
+}
