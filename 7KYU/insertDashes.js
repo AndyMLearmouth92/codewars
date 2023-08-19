4e704650935e3447ceaa15057e2ac467c651aba2
Andy Learmouth <andymlearmouth@hotmail.com>
2023-08-19 19:04:33 +0100

    Insert dashes CW Solution S1

---
+++ 7KYU/insertDashes.js
@@ -0,0 +1,16 @@
+/* Write a function insert_dash(num) / insertDash(num) / InsertDash(int num) that will insert dashes ('-') between each two odd digits in num. For example: if num is 454793 the output should be 4547-9-3.
+
+Note that the number will always be non-negative (>= 0).
+
+Starter code:
+
+function insertDash(num) {
+   //code me
+}
+*/
+
+// Solution 1:
+
+function insertDash(num) {
+    return num.toString().replace(/[13579](?=[13579])/g, "$&-");
+ }
