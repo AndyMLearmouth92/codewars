9d4492a6a7d968c0482511d5ac325a797730d5d6
Andy Learmouth <andymlearmouth@hotmail.com>
2023-08-19 19:05:09 +0100

    Insert dashes CW Solution S2

--- 7KYU/insertDashes.js
+++ 7KYU/insertDashes.js
@@ -14,3 +14,10 @@ function insertDash(num) {
 function insertDash(num) {
     return num.toString().replace(/[13579](?=[13579])/g, "$&-");
  }
+
+// Solution 2:
+
+function insertDash(num) {
+    let arr = String(num).split('')
+    return arr.map((e,i,arr) => arr[i] % 2 !== 0 && (arr[i+1] % 2 !== 0 && i !== arr.length-1) ? e+'-' : e).join('')
+ }
