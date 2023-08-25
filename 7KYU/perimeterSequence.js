e3061f76eeda337d2d1636f4d3867a84d83921d0
Andy Learmouth <andymlearmouth@hotmail.com>
2023-08-25 19:17:53 +0100

    Perimeter sequence CW solution

---
+++ 7KYU/perimeterSequence.js
@@ -0,0 +1,22 @@
+/* Perimeter sequence
+
+The first three stages of a sequence are shown.
+
+blocks
+
+The blocksize is a by a and a ≥ 1.
+
+What is the perimeter of the nth shape in the sequence (n ≥ 1) ?
+
+Starter code:
+
+function perimeterSequence(a,n) {
+  // your code
+}
+*/
+
+// Solution:
+
+function perimeterSequence(a, n) {
+  return n * (a * 4);
+}
