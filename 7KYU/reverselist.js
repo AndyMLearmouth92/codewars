0cf4a9c9380627fa4b96ec9c719bc206059c013c
Andy Learmouth <andymlearmouth@hotmail.com>
2023-09-03 19:42:09 +0100

    Reverse list CW solution

---
+++ 7KYU/reverselist.js
@@ -0,0 +1,14 @@
+/* Write reverseList function that simply reverses lists.
+
+Starter code:
+
+function reverseList(arr) {
+    return arr;
+  }
+  */
+
+// Solution:
+
+function reverseList(arr) {
+  return arr;
+}
