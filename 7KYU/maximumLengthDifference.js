c5ec73f02eeea4b8d1d39946dad144ab573b2ba1
Andy Learmouth <andymlearmouth@hotmail.com>
2023-09-09 16:18:47 +0100

    Maximum Length Difference CW S2.

--- 7KYU/maximumLengthDifference.js
+++ 7KYU/maximumLengthDifference.js
@@ -31,3 +31,17 @@ function mxdiflg(a1, a2) {
     );
   }
 }
+
+// Solution 2:
+
+function mxdiflg(a1, a2) {
+  if (!a1.length || !a2.length) {
+    return -1;
+  }
+  a1.sort((a, b) => a.length - b.length);
+  a2.sort((a, b) => a.length - b.length);
+  return Math.max(
+    Math.abs(a1[0].length - a2[a2.length - 1].length),
+    Math.abs(a2[0].length - a1[a1.length - 1].length)
+  );
+}
