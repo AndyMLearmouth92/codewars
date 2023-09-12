7b9e5de42a978e2bf2ca1cd5c79f87f4e85d1c43
Andy Learmouth <andymlearmouth@hotmail.com>
2023-09-12 17:09:43 +0100

    Collatz Conjecture CW Solution 3

--- 7KYU/collatzConjectureLength.js
+++ 7KYU/collatzConjectureLength.js
@@ -46,3 +46,15 @@ function collatz(n) {
   }
   return i;
 }
+
+// Solution 3:
+
+function collatz(n) {
+  if (n === 1) {
+    return 1;
+  } else if (n % 2 === 0) {
+    return 1 + collatz(n / 2);
+  } else {
+    return 1 + collatz(n * 3 + 1);
+  }
+}
