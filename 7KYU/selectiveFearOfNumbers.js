b45e9cdf026d0efff7e7f40356a36b17b922eb97
Andy Learmouth <andymlearmouth@hotmail.com>
2023-09-23 10:47:57 +0100

    Selective fear of numbers CW Solution.

--- 7KYU/selectiveFearOfNumbers.js
+++ 7KYU/selectiveFearOfNumbers.js
@@ -44,3 +44,25 @@ var AmIAfraid = function (day, num) {
     return false;
   }
 };
+
+// Solution 2:
+
+function AmIAfraid(day, num) {
+  switch (day) {
+    case "Monday":
+      return num == 12;
+    case "Tuesday":
+      return num > 95;
+    case "Wednesday":
+      return num == 34;
+    case "Thursday":
+      return num == 0;
+    case "Friday":
+      return num % 2 == 0;
+    case "Saturday":
+      return num == 56;
+    case "Sunday":
+      return num == 666 || num == -666;
+  }
+  return false;
+}
