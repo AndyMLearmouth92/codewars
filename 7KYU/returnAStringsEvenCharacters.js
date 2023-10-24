6e41e593df285e8afef6dc1c18d69f9ac08046df
Andy Learmouth <andymlearmouth@hotmail.com>
2023-10-24 17:49:14 +0100

    Return a string's even characters CW S1.

---
+++ 7KYU/returnAStringsEvenCharacters.js
@@ -0,0 +1,25 @@
+/* Write a function that returns a sequence (index begins with 1) of all the even characters from a string. If the string is smaller than two characters or longer than 100 characters, the function should return "invalid string".
+
+For example:
+
+"abcdefghijklm" --> ["b", "d", "f", "h", "j", "l"]
+"a"             --> "invalid string"
+
+Starter code:
+
+function evenChars(string) {
+//keep coding!
+}
+
+*/
+
+// Solution:
+
+function evenChars(string) {
+    let inputArr = string.split('')
+    if(inputArr.length < 2 || inputArr.length > 100){
+      return 'invalid string'
+    }else{
+      return inputArr.filter((e,i) => i % 2 !== 0)
+    }
+  }
