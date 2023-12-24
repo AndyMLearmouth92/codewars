c6ec1bf432636c63ace6e8964ef1f71bf9e7fe96
Andy Learmouth <andymlearmouth@hotmail.com>
2023-12-24 14:44:39 +0000

    Filter Long Words

---
+++ 7KYU/filterLongWords.js
@@ -0,0 +1,20 @@
+/* Write a function that takes a string and an an integer n as parameters and returns a list of all words that are longer than n.
+
+Example:
+
+* With input "The quick brown fox jumps over the lazy dog", 4
+* Return ['quick', 'brown', 'jumps']
+
+Starter code:
+
+function filterLongWords(sentence, n) {
+    //Solution here
+  }
+
+*/
+
+// Solution:
+
+function filterLongWords(sentence, n) {
+  return sentence.split(" ").filter((e) => e.length > n);
+}
