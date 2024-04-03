6bbadd1852520a0e65c22d5a9b25583ff7adaf12
Andy Learmouth <andymlearmouth@hotmail.com>
2024-04-03 19:45:18 +0100

    Area of an arrow CWS1.

---
+++ 7KYU/areaOfAnArrow.js
@@ -0,0 +1,18 @@
+/* Area of an arrow
+
+An arrow is formed in a rectangle with sides a and b by joining the bottom corners to the midpoint of the top edge and the centre of the rectangle.
+
+a and b are integers and > 0
+
+Write a function which returns the area of the arrow.
+
+Starter code:
+
+function arrowArea(a,b) {
+  // your code
+}
+*/
+
+// Solution:
+
+const arrowArea = (a, b) => (a * b) / 4;
