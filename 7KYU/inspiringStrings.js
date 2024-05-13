fcaca757ac1e662ba81d79171637179949e544d0
Andy Learmouth <andymlearmouth@hotmail.com>
2024-05-13 20:00:26 +0100

    Inspiring Strings CWS1.

---
+++ 7KYU/inspiringStrings.js
@@ -0,0 +1,19 @@
+/* Given a string of space separated words, return the longest word.
+If there are multiple longest words, return the rightmost longest word.
+
+Examples
+
+"red white blue"  =>  "white"
+"red blue gold"   =>  "gold"
+
+Starter code:
+
+function longestWord(stringOfWords) {
+  // Give me back the longest word!
+}
+*/
+
+// Solution 1:
+
+const longestWord = (s) =>
+  s.split(" ").reduceRight((a, b) => (b.length > a.length ? b : a));
