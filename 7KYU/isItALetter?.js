fc4e7b901c8d872f2136262d82fb7286d14ce711
Andy Learmouth <andymlearmouth@hotmail.com>
2024-07-25 19:18:01 +0100

    Is it a letter? CW S1.

---
+++ 7KYU/isItALetter?.js
@@ -0,0 +1,18 @@
+/* Write a function, isItLetter or is_it_letter or IsItLetter, which tells us if a given character is a letter or not.
+
+Starter code:
+
+function isItLetter(character) {
+  return true || false ;
+}
+
+*/
+
+// Solution 1:
+
+function isItLetter(character) {
+  if (/[a-zA-Z]/.test(character)) {
+    return true;
+  }
+  return false;
+}
