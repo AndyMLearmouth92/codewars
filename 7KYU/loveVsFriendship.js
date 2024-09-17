83c7e3fe7786c22a4bc5423b7cf6f3a8eb97320a
Andy Learmouth <andymlearmouth@hotmail.com>
2024-09-17 17:40:38 +0100

    Love vs friendship CWS1.

---
+++ 7KYU/loveVsFriendship.js
@@ -0,0 +1,26 @@
+/* If　a = 1, b = 2, c = 3 ... z = 26
+
+Then l + o + v + e = 54
+
+and f + r + i + e + n + d + s + h + i + p = 108
+
+So friendship is twice as strong as love :-)
+
+Your task is to write a function which calculates the value of a word based off the sum of the alphabet positions of its characters.
+
+The input will always be made of only lowercase letters and will never be empty.
+
+Starter code:
+
+function wordsToMarks(string){
+  //your code here
+}
+*/
+
+// Solution 1:
+
+function wordsToMarks(string) {
+  return string
+    .split("")
+    .reduce((sum, current) => sum + current.charCodeAt() - 96, 0);
+}
