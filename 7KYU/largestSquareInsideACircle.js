04b6082002c1d68b7fd11b95cbe787e55a58d2c8
Andy Learmouth <andymlearmouth@hotmail.com>
2024-09-24 17:42:12 +0100

    Largest Square Inside A Circle CWS1.

---
+++ 7KYU/largestSquareInsideACircle.js
@@ -0,0 +1,12 @@
+/* Determine the area of the largest square that can fit inside a circle with radius r.
+
+Starter code:
+
+function areaLargestSquare(r) {
+  return ; //code here
+}
+*/
+
+// Solution:
+
+const areaLargestSquare = (r) => Math.pow(r, 2) * 2;
